693e3813b6422bf16957998b88b05612ae1d6bf7
Ana Julia Terra <ajtalves@hotmail.com>
2021-01-28 17:40:27 -0300

    Correções das Models

--- app/Episodio.php
+++ app/Episodio.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Episodio extends Model
 {
+
+    public $timestamps = false;
+    protected $fillable = ['numero'];
+    
     public function temporada()
     {
         return $this->belongsTo(Temporada::class);
--- app/Temporada.php
+++ app/Temporada.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Temporada extends Model
 {
+
+    public $timestamps = false;
+    protected $fillable = ['numero'];
+    
     public function episodios()
     {
         return $this->hasMany(Episodio::class); // relação de episodios com uma temporada
