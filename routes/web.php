41f93b44fd0c31f7e93f7040ae0566750664d7e6
Ana Julia Terra <ajtalves@hotmail.com>
2021-01-31 14:05:12 -0300

    Criando rotas pra manipular episódios
    Aula 5

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::post('/series/criar', 'SeriesController@store');
 Route::delete('/series/remover/{id}', 'SeriesController@destroy');
 Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
 Route::post('/series/{id}/editaNome', 'SeriesController@editaNome');
+Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
+Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir');
